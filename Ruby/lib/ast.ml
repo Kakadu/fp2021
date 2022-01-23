1dd4d1d51cd67704b585fd2faa8eb29c4062336f
SofuriHafe <dinis.taranov@mail.ru>
2022-01-23 22:54:32 +0300

    docs & class/func/return fix & tests & refactoring

--- Ruby/lib/ast.ml
+++ Ruby/lib/ast.ml
@@ -5,7 +5,7 @@ type value =
   | Boolean of bool
 [@@deriving show { with_path = false }]
 
-type identifier = None | Identifier of string
+type identifier = Null | Identifier of string
 [@@deriving show { with_path = false }]
 
 type modifier = Local | Instance | Global | Class
@@ -28,8 +28,9 @@ type expression =
   | Or of expression * expression
   | List of expression list
   | Call of identifier * identifier * expression list
-  | Lambda of expression list * statement list
-  | Nil
+      (** (name of the parent: class OR "Null" in case of stand-alone func) (name of the called func) [expr list] *)
+  | Lambda of expression list * statement list  (** [var list] [stmt list] *)
+  | Nil  (** "null" *)
 [@@deriving show { with_path = false }]
 
 and statement =
@@ -37,9 +38,11 @@ and statement =
   | Assign of expression * expression
   | Return of expression
   | IfElse of expression * statement list * statement list
+      (** (expr) [stmt list] [stmt list OR [] in case of "else" absence] *)
   | While of expression * statement list
   | Class of identifier * statement list
   | Function of identifier * expression list * statement list
+      (** (id) [args list] [stmt list] *)
   | Break
-  | Next
+  | Next  (** "continue" *)
 [@@deriving show { with_path = false }]
--- Ruby/lib/parser.ml
+++ Ruby/lib/parser.ml
@@ -58,24 +58,29 @@ let e_or e1 e2 = Or (e1, e2)
 let e_list el1 = List el1
 let e_lambda el1 sl1 = Lambda (el1, sl1)
 let e_func_call i1 i2 el1 = Call (i1, i2, el1)
-let ps_assign e1 e2 = Assign (e1, e2)
-let ps_expression e1 = Expression e1
-let ps_return e1 = Return e1
-let ps_if_else e1 sl1 sl2 = IfElse (e1, sl1, sl2)
-let ps_while e1 sl1 = While (e1, sl1)
-let ps_class i1 sl1 = Class (i1, sl1)
-let ps_func i1 el1 sl1 = Function (i1, el1, sl1)
+let e_nil = Nil
+let e_true = Constant (Boolean true)
+let e_false = Constant (Boolean false)
+let s_break = Break
+let s_next = Next
+let s_assign e1 e2 = Assign (e1, e2)
+let s_expression e1 = Expression e1
+let s_return e1 = Return e1
+let s_if_else e1 sl1 sl2 = IfElse (e1, sl1, sl2)
+let s_while e1 sl1 = While (e1, sl1)
+let s_class i1 sl1 = Class (i1, sl1)
+let s_func i1 el1 sl1 = Function (i1, el1, sl1)
 let i_identifier i1 = Identifier i1
 
 (* Lifters *)
-let lift_return = lift ps_return
-let lift_class = lift2 ps_class
+let lift_return = lift s_return
+let lift_class = lift2 s_class
 let lift_lambda = lift2 e_lambda
-let lift_assign = lift2 ps_assign
-let lift_while = lift2 ps_while
-let lift_func = lift3 ps_func
+let lift_assign = lift2 s_assign
+let lift_while = lift2 s_while
+let lift_func = lift3 s_func
 let lift_func_call = lift3 e_func_call
-let lift_if_else = lift3 ps_if_else
+let lift_if_else = lift3 s_if_else
 
 (* Tokens *)
 let t_comma = token ","
@@ -89,9 +94,28 @@ let t_def = token "def"
 let t_class = token "class"
 let t_lambda = token "lambda"
 let t_return = token "return"
+let t_nil = token "nil"
 let t_instance_v = token "@"
 let t_global_v = token "$"
 let t_class_v = token "@@"
+let t_break = token "break"
+let t_next = token "next"
+let t_true = token "true"
+let t_false = token "false"
+let t_add = token "+"
+let t_sub = token "-"
+let t_mul = token "*"
+let t_div = token "/"
+let t_mod = token "%"
+let t_eq = token "=="
+let t_gr = token ">"
+let t_gr_eq = token ">="
+let t_less = token "<"
+let t_less_eq = token "<="
+let t_and = token "and"
+let t_and_1 = token "&&"
+let t_or = token "or"
+let t_or_1 = token "||"
 
 (* Chains *)
 let chainl1 e op =
@@ -99,25 +123,25 @@ let chainl1 e op =
   e >>= fun init -> go init
 
 (* Single Parsers *)
-let p_add = token "+" *> return e_add
-let p_sub = token "-" *> return e_sub
-let p_mul = token "*" *> return e_mul
-let p_div = token "/" *> return e_div
-let p_mod = token "%" *> return e_mod
-let p_eq = token "==" *> return e_eq
-let p_gr = token ">" *> return e_gr
-let p_gr_eq = token ">=" *> return e_gr_eq
-let p_less = token "<" *> return e_less
-let p_less_eq = token "<=" *> return e_less_eq
-let p_and = token "and" *> return e_and
-let p_and_1 = token "&&" *> return e_and
-let p_or = token "or" *> return e_or
-let p_or_1 = token "||" *> return e_or
-let p_break = token "break" *> return Break
-let p_next = token "next" *> return Next
-let p_nil = token "nil" *> return Nil
-let p_true = token "true" *> return (Constant (Boolean true))
-let p_false = token "false" *> return (Constant (Boolean false))
+let p_add = t_add *> return e_add
+let p_sub = t_sub *> return e_sub
+let p_mul = t_mul *> return e_mul
+let p_div = t_div *> return e_div
+let p_mod = t_mod *> return e_mod
+let p_eq = t_eq *> return e_eq
+let p_gr = t_gr *> return e_gr
+let p_gr_eq = t_gr_eq *> return e_gr_eq
+let p_less = t_less *> return e_less
+let p_less_eq = t_less_eq *> return e_less_eq
+let p_and = t_and *> return e_and
+let p_and_1 = t_and_1 *> return e_and
+let p_or = t_or *> return e_or
+let p_or_1 = t_or_1 *> return e_or
+let p_nil = t_nil *> return e_nil
+let p_break = t_break *> return s_break
+let p_next = t_next *> return s_next
+let p_true = t_true *> return e_true
+let p_false = t_false *> return e_false
 
 let p_identifier =
   take_variable >>= function
@@ -166,16 +190,16 @@ let p_lambda el1 sl1 =
 
 let p_func_call el1 =
   lift_func_call p_identifier (take_dot *> p_identifier) (round_brackets el1)
-  <|> lift_func_call (return None) p_identifier (round_brackets el1)
+  <|> lift_func_call (return Null) p_identifier (round_brackets el1)
 
 let p_func el1 sl1 =
-  let i = t_def *> p_identifier <* skip_stmt_sep in
+  let i = t_def *> skip_whitespace *> p_identifier <* skip_stmt_sep in
   lift_func i
     (round_brackets el1 <* skip_stmt_sep <|> return [])
     (sl1 <* skip_stmt_sep <* t_end)
 
 let p_class sl1 =
-  let i = t_class *> p_identifier <* skip_stmt_sep in
+  let i = t_class *> skip_whitespace *> p_identifier <* skip_stmt_sep in
   lift_class i sl1 <* skip_stmt_sep <* t_end
 
 let p_while el1 sl1 =
@@ -188,7 +212,7 @@ let p_if_else el1 sl1 =
     (t_else *> sl1 <* skip_stmt_sep <|> return [] <* t_end)
 
 let p_return el1 =
-  let e = t_return *> skip_whitespace *> el1 in
+  let e = t_return *> skip_whitespace *> (el1 <|> return e_nil) in
   lift_return e
 
 let p_assign el1 = lift_assign (el1 <* t_assign) el1
@@ -200,6 +224,7 @@ let gp_compare_op = p_eq <|> p_gr_eq <|> p_less_eq <|> p_gr <|> p_less
 let gp_logic_op = p_and <|> p_and_1 <|> p_or <|> p_or_1
 let gp_number = p_float <|> p_integer
 let gp_pseudo = p_true <|> p_false <|> p_nil
+let gp_loop_jumps = p_break <|> p_next
 
 let gp_variable =
   p_class_variable <|> p_global_variable <|> p_instance_variable
@@ -237,9 +262,7 @@ let bundle =
     fix @@ fun p_statement ->
     let expression_list = sep_by t_comma (duo.p_expression duo) in
     let statement_list = sep_by skip_stmt_sep p_statement in
-    let ps_expression = duo.p_expression duo >>| ps_expression in
-    let ps_break = p_break in
-    let ps_next = p_next in
+    let ps_expression = duo.p_expression duo >>| s_expression in
     let ps_assign = p_assign (duo.p_expression duo) in
     let ps_return = p_return (duo.p_expression duo) in
     let ps_if_else = p_if_else (duo.p_expression duo) statement_list in
@@ -247,8 +270,8 @@ let bundle =
     let ps_class = p_class statement_list in
     let ps_func = p_func expression_list statement_list in
     skip_stmt_sep
-    *> (ps_assign <|> ps_return <|> ps_break <|> ps_next <|> ps_if_else
-      <|> ps_while <|> ps_class <|> ps_func <|> ps_expression)
+    *> (ps_assign <|> ps_return <|> gp_loop_jumps <|> ps_if_else <|> ps_while
+      <|> ps_class <|> ps_func <|> ps_expression)
   in
   { p_expression; p_statement }
 
