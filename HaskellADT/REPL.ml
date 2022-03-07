75ebce4fe2494d6e9c79cbe2bf4393f967d58c2f
esvault <azarnikov01@gmail.com>
2022-03-07 22:36:13 +0300

    First essential commit

--- HaskellADT/REPL.ml
+++ HaskellADT/REPL.ml
@@ -1,4 +1,25 @@
-(* open Haskell_lib.Ast *)
+open HaskellADT_lib.Interpreter
+open HaskellADT_lib.Parser
 
 (* let x = Var "a" *)
 let () = print_endline "REPL not implemented"
+
+let code = {|
+let x = 2 + 5
+let f x y = x + y
+let p = f 5 10
+let b = (True && (5 > 4))
+let xs = [1,2,3,4]
+let x = "Hello" <> "Hello"
+|}
+
+let funct code = 
+  match run_interpret code with
+  | Ok ok -> (Caml.Format.printf "%a\n" pp_interpret_ok ok) 
+  | Error err -> (Caml.Format.printf "%a\n" pp_interpret_err err)
+;;
+
+(* let rez = funct code;; *)
+
+(* parse_or_error {|let f x = case x of 0 -> True 
+                                     1 -> False|} *)
---
+++ HaskellADT/lib/ast.ml
@@ -0,0 +1,84 @@
+type id = string [@@deriving eq, show {with_path=false}]
+
+type bin_op = 
+  | Add (**  +   *)
+  | Sub (**  -   *)
+  | Mul (**  *   *)
+  | Div (**  /   *)
+  | LT  (**  <   *)
+  | LE  (**  <=  *)
+  | GT  (**  >   *)
+  | GE  (**  >=  *)
+  | NE  (**  /=  *)
+  | EQ  (**  ==  *)
+  | And (**  &&  *)
+  | Or  (**  ||  *)
+[@@deriving eq, show {with_path=false}]
+
+and un_op = 
+  | Minus (** -   *)
+  | Not   (** not *)
+[@@deriving eq, show {with_path=false}]
+
+and const = 
+  | CBool of bool      (** True    *)
+  | CInt of int        (** 201     *)
+  | CString of string  (** "OCaml" *)
+[@@deriving eq, show {with_path=false}]
+
+and expr = 
+  | EConst of const                              (**  1  *)
+  | EBinOp of bin_op * expr * expr               (**  201 - 1  *)
+  | EUnOp of un_op * expr                        (**  not True  *)
+  | EVar of id                                   (**  a  *)
+  | ETuple  of expr list                         (**  ("Tom", "Hardy", 44)  *)
+  | ECons of expr * expr                         (**  h : tl  *)
+  | ENull                                        (**  []  *)
+  | EApp of expr * expr                          (**  f x y  = (f x) y*)
+  | EIf of expr * expr * expr                    (**  if x >= 5 then "Nice" else "Bad"  *)
+  | ELet of binding * expr                  (**  let x = 5; y = 10 in x + y  *)
+  | ELstCompr of expr * fromSet list * expr list (**  [z | x <- xs, let y = x + 1, z = x * y]  *)
+  | ECase of expr * case list                    (**  head xs = case xs of [] -> -100
+                                                                           (x:_) -> x  *)
+  | ECtor of id * expr
+  | EFun of pat * expr                           (**  \x -> x + 10  *)
+[@@deriving eq, show {with_path=false}]
+
+and case = pat * expr  (**  _ -> 100*)
+and guard = expr * expr  (**  | a < 5 = 10  *)
+and actor = id * tyexpr  (**  Point Int Int  *)
+and fromSet = expr * expr [@@deriving eq, show {with_path=false}]
+    (**  x <- [1,2,3,4,5,6]  *)
+and binding = pat * expr [@@deriving eq, show {with_path=false}]
+    (**  fact 0 = 1
+         fact n = n * fact (n - 1)*)
+
+and pat = 
+  | PWild  (**  _  *)
+  | PConst of const  (** True  *)
+  | PVar of id (**  x  *)
+  | PCons of pat * pat  (** hd : tl  *)
+  | PNull  (**  []  *)
+  | PTuple of pat list  (**  (_, 1, "A")  *)
+  | PACase of id * pat  (**  Point 3 0  *)
+[@@deriving eq, show {with_path=false}]
+
+and tyexpr = | TInt
+  | TBool
+  | TString
+  | TList of tyexpr
+  | TTuple of tyexpr list
+  | TArrow of tyexpr * tyexpr  (** string -> string *)
+  | TAdt of id
+[@@deriving eq, show {with_path=false}]
+
+and adt = id * actor list
+
+and decl = 
+  | DLet of binding
+  | DAdt of adt
+[@@deriving eq, show {with_path=false}]
+
+and prog = decl list [@@deriving eq, show {with_path=false}] 
+
+
