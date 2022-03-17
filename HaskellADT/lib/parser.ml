a770ae5a9e57241217250292e0b023abb5c36277
esvault <azarnikov01@gmail.com>
2022-03-17 23:11:17 +0300

    fixed pattern parsing

--- HaskellADT/lib/parser.ml
+++ HaskellADT/lib/parser.ml
@@ -14,10 +14,6 @@ let is_ws = function
  | _ -> false
 
 let is_eol = function
-
-
-
-
  | '\r' | '\n' -> true
  | _ -> false
 
@@ -70,6 +66,7 @@ let efun args rhs =
 let ccase p e = (p, e)
 let acase id p = (id, p)
 let pacase id p = PACase (id, p)
+let actor id ty = (id, ty)
 let bbind p e = (p, e)
 
 let pwild _ = PWild
@@ -81,6 +78,7 @@ let pcons = return @@ fun p1 p2 -> PCons (p1, p2)
 let plist = List.fold_right ~f:(fun p1 p2 -> PCons (p1, p2)) ~init:PNull
 
 let dlet p e = DLet (p, e)
+let dadt i a = DAdt (i, a)
 
 let tint = TInt
 let tbool = TBool
@@ -121,7 +119,7 @@ let id valid_fst =
   in
   let* inner = take_func @@ satisfy is_idchar in
   let id = Base.String.of_char_list @@ (fst :: inner) in
-  if is_keyword id then fail "Keyword" else return id
+  if is_keyword id then fail @@ "Keyword" else return id
 ;;
 
 let ident =
@@ -142,17 +140,13 @@ let capitalized_ident =
 let cuint = 
   trim (take_while1 is_digit) 
   >>= fun num -> return (Int.of_string num) >>| cint 
-
+ 
 let cint = 
   trim (option "" (token "+" <|> token "-"))
   >>= fun sign -> take_while1 is_digit
   >>= fun num -> return (Int.of_string (sign ^ num)) >>| cint
 
 let cbool = 
-
-
-
-  
   let ctrue = kwd "True" *> return (cbool true) in
   let cfalse = kwd "False" *> return (cbool false) in
   ctrue <|> cfalse
@@ -168,16 +162,16 @@ let pvar = ident >>| pvar
 let pwild = token "_" >>| pwild
 let pconst = const >>| pconst
 
-type pdispatch =
-  { tuple: pdispatch -> pat t
-  ; cons: pdispatch -> pat t
-  ; pat: pdispatch -> pat t
-  ; adt: pdispatch -> pat t }
+(* type pdispatch =
+{ tuple: pdispatch -> pat t
+; cons: pdispatch -> pat t
+; pat: pdispatch -> pat t
+; adt: pdispatch -> pat t }
 
 let pack = 
   let pat d = fix @@ fun _self -> trim @@ choice [d.tuple d; d.cons d; d.adt d] in
   let tuple d = 
-    fix @@ fun _self ->
+    fix @@ fun _self -> 
       trim @@ lift2 (fun hd tl -> hd::tl) (d.cons d) (many1 (comma *> d.cons d))
       >>| ptuple in
   let cons d = fix @@ fun _self -> chainr1 (d.adt d) popcons in
@@ -186,10 +180,38 @@ let pack =
       trim @@ between lsb rsb (sep_by comma (d.pat d)) >>| plist in
     let prim =
       trim @@ choice [pconst; pvar; pwild; plist; parens @@ d.pat d] in
+    let help = lift2 pacase constr_ident (option (PTuple []) (many (empty1 *> d.adt d) >>| function [x] -> x | lst -> PTuple lst)) in
     trim
-      ( lift2 pacase constr_ident (option (PTuple []) (empty1 *> d.adt d))
-      <|> prim ) in
+      (prim <|> (parens @@ help) <|> help) in
   {tuple; cons; pat; adt}
+let pattern = pack.pat pack *)
+
+type pdispatch =
+  { tuple: pdispatch -> pat t
+  ; adt: pdispatch -> pat t
+  ; other: pdispatch -> pat t
+  ; pat: pdispatch -> pat t
+  }
+
+let pack =
+  let pat d = fix @@ fun _self -> trim @@ choice [ d.tuple d; d.other d; d.adt d ] in
+  let tuple d =
+    fix
+    @@ fun _self ->
+    trim @@ lift2 (fun hd tl -> hd :: tl) (d.other d) (many1 (comma *> d.other d))
+    >>| ptuple
+  in
+  let adt d = fix @@ fun _self ->
+    trim @@ lift2 pacase constr_ident (option (PTuple []) (many (empty1 *> d.pat d) >>| function [x] -> x | lst -> PTuple lst))
+  in
+  let other d =
+    fix
+    @@ fun _self ->
+    let plist = trim @@ between lsb rsb @@ sep_by semi @@ d.pat d >>| plist in
+    let prim = trim @@ choice [ pconst; pvar; pwild; plist; parens @@ d.pat d ] in
+    trim @@ chainr1 prim popcons
+  in
+  { tuple; adt; other; pat }
 
 let pattern = pack.pat pack
 
@@ -205,33 +227,32 @@ let app_unop p =
 ;;
 
 let pack = 
-  let expr d = fix (fun _self -> trim @@ d.key d <|> d.tuple d <|> d.op d) in
-  let key d = fix (fun _self ->
+  let expr d = fix @@ fun _self -> trim @@ d.key d <|> d.tuple d <|> d.op d in
+  let key d = fix @@ fun _self ->
     let eif = trim (lift3 eif
                       (kwd "if" *> d.expr d)
                       (kwd "then" *> d.expr d)
                       (kwd "else" *> d.expr d)) in
     let elet = 
-      let binding = trim (lift2 bbind pattern 
-                                      (lift2 efun (empty *> many pattern <* token "=") (d.expr d <* kwd "in")))
+      let binding = trim (lift2 bbind (token "let" *> pattern) 
+                                      (lift2 efun (empty1 *> many pattern <* token "=") (d.expr d <* kwd "in")))
       in
-      trim (lift2 elet (binding) (d.expr d))
+      trim (lift2 elet binding (d.expr d))
     in
     let efun = trim (lift2 efun (kwd "\\" *> many pattern <* arrow) (d.expr d)) in
     let ecase = 
-      let case1 = trim @@ lift2 ccase (empty *> pattern <* arrow) (d.expr d) in
-      let casen = trim @@ lift2 ccase (empty *> pattern <* arrow) (d.expr d) in
-      let cases = trim @@ lift2 (fun fst other -> fst :: other) (case1) (many casen) 
+      let case = trim @@ lift2 ccase (empty *> pattern <* arrow) (d.expr d) in
+      let cases = trim @@ lift2 (fun fst other -> fst :: other) (case) (many case) 
       in
       trim (lift2 ecase (kwd "case" *> empty1 *> d.expr d <* kwd "of") cases) in
-    choice [elet; eif; efun; ecase])
+    choice [elet; eif; efun; ecase]
   in
   let tuple d = lift2 (@) (many1 (d.op d <* comma)) (d.op d <|> d.key d >>| fun rhs -> [rhs]) >>| etuple
   in
   let op d = fix @@ fun _self -> 
     let lst = trim @@ between lsb rsb @@ sep_by comma (d.expr d) in
     let prim = trim @@ choice [lst >>| elist; uconst >>| econst; ident >>| evar
-                              ; parens @@ d.expr d] in
+                              ; parens @@ d.expr d; (constr_ident >>| fun s -> ECtor (s, ETuple []))] in
     let procl op pl pr = 
       let rec go acc = 
         lift2 (fun f x -> f acc x) op (choice [pl >>= go; pr]) <|> return acc 
@@ -252,11 +273,27 @@ let pack =
 
 let expr = pack.expr pack
 
+let tyexpr = 
+  fix @@ fun tyexpr -> 
+    let prim = trim @@ choice
+      [ token "Int" *> return TInt; token "String" *> return TString;
+        token "Bool" *> return TBool; constr_ident >>| (fun i -> TAdt i); parens tyexpr] in
+    let list = lsb *> prim <* rsb >>| (fun lst -> TList lst) in
+    let total = many1 ((list <|> prim) <* empty)
+    >>| function [x] -> x | tpl -> TTuple tpl in
+  trim @@ chainr1 total (arrow *> return (fun t1 t2 -> TArrow (t1, t2)))
+
 let decl = 
   let dlet = lift2 dlet
     (kwd "let" *> empty1 *> pattern)
     (lift2 efun (empty1 *> many pattern <* token "=") expr) in
-  trim @@ dlet
+  let dadt = 
+    let actor_fst = lift2 actor (capitalized_ident) (option (TTuple []) (empty1 *> tyexpr)) in 
+    let actor_other = lift2 actor (token "|" *> capitalized_ident)  (option (TTuple []) (empty1 *> tyexpr)) in
+    lift2 dadt
+    (kwd "data" *> empty1 *> capitalized_ident <* token "=")
+    (lift2 (fun fst other -> fst :: other) actor_fst (many actor_other)) in
+  trim @@ dlet <|> dadt
 
 let prog = many1 (trim @@ decl <* trim (many (trim (token ";;"))) <|> decl)
 let parse_with p s = parse_string ~consume:Consume.All p s
@@ -300,9 +337,10 @@ let rez = parse_or_error code;;
 
 (* let rez = Caml.Format.printf "%a\n" pp_prog rez *)
 
-
 let () = 
-  let code = "let (x,xs) = (1,2) let (x:xs) = xs" in 
+  let code = "
+  let a = 
+  4 * 10" in 
   match parse_with prog code with
   | Ok ok -> Format.printf "%a\n" pp_prog ok
   | Error err -> Format.printf "%s\n" err
@@ -310,23 +348,98 @@ let () =
 
 let%test _ =
   parse_test {|
-  let x = 2
-  let y = x + 10 / 5
-|} [DLet (PVar ("x"), EConst (CInt (2)));
-    DLet (PVar ("y"), 
-          EBinOp 
-          (Add, EVar ("x"), 
-          EBinOp (Div, EConst (CInt (10)), EConst (CInt (5)))))]
+    let x = 2
+    let _ = x + ((10 + x * x)/700 + 10 / 5) - (x - 1)/x
+  |} [DLet ((PVar ("x"), EConst (CInt (2))));
+      DLet ((PWild,
+             EBinOp
+             (Add, EVar ("x"),
+              EBinOp
+              (Sub,
+               EBinOp
+               (Add,
+                EBinOp
+                (Div,
+                 EBinOp
+                 (Add, EConst (CInt (10)), EBinOp (Mul, EVar ("x"), EVar ("x"))),
+                 EConst (CInt (700))),
+                EBinOp (Div, EConst (CInt (10)), EConst (CInt (5)))),
+               EBinOp
+               (Div, EBinOp (Sub, EVar ("x"), EConst (CInt (1))), EVar ("x"))))))]
 
 let%test _ = 
   parse_test {|
-  let f x y = x * y
-  let f5 x = f 5 x
-  |} [DLet (PVar ("f"),
-      EFun (PVar ("x"), 
-      EFun (PVar ("y"), 
-      EBinOp (Mul, EVar("x"), EVar("y")))));
-      DLet (PVar ("f5"), 
-      EFun (PVar ("x"), 
-      EApp (EApp (EVar ("f"), 
-      EConst (CInt (5))), EVar ("x"))))]
+    let f x y = x * y
+    let f5 x = f 5 x
+  |} [DLet ((PVar ("f"),
+             EFun
+             (PVar ("x"), EFun (PVar ("y"), EBinOp (Mul, EVar ("x"), EVar ("y"))))));
+      DLet ((PVar ("f5"),
+             EFun
+             (PVar ("x"), EApp (EApp (EVar ("f"), EConst (CInt (5))), EVar ("x")))))]
+
+let%test _ = 
+  parse_test {|
+    let rect_surface x1 y1 x2 y2 = 
+      let abs x = if x > 0 then x else 0 - x in
+      (abs (x1 - x2)) * (abs (y1 - y2)) 
+  |} [DLet ((PVar ("rect_surface"),
+             EFun
+             (PVar ("x1"),
+              EFun
+              (PVar ("y1"),
+               EFun
+               (PVar ("x2"),
+                EFun
+                (PVar ("y2"),
+                 ELet
+                 ((PVar ("abs"),
+                   EFun
+                   (PVar ("x"),
+                    EIf
+                    (EBinOp (GT, EVar ("x"), EConst (CInt (0))), EVar ("x"),
+                     EBinOp (Sub, EConst (CInt (0)), EVar ("x"))))),
+                  EBinOp
+                  (Mul,
+                   EApp (EVar ("abs"), EBinOp (Sub, EVar ("x1"), EVar ("x2"))),
+                   EApp (EVar ("abs"), EBinOp (Sub, EVar ("y1"), EVar ("y2")))))))))))]
+
+let%test _ = 
+    parse_test {|
+      let ( ( e , s ) )      =      ( 1 ,  2 )
+      let ( x , y , z , ( a , b ) ) = ( 1 , 2 , 3 , ( 4 , 5 ) )
+    |} [DLet ((PTuple ([PVar ("e"); PVar ("s")]),
+               ETuple ([EConst (CInt (1)); EConst (CInt (2))])));
+        DLet ((PTuple ([PVar ("x"); PVar ("y"); PVar ("z");
+                        PTuple ([PVar ("a"); PVar ("b")])]),
+               ETuple ([EConst (CInt (1)); EConst (CInt (2)); EConst (CInt (3));
+                        ETuple ([EConst (CInt (4)); EConst (CInt (5))])])))]
+
+let%test _ = 
+    parse_test {|
+      data Point = Point Int Int
+      data Shape = Rectangle Point Point | Circle Point Int
+      let s1 = Rectangle (Point 0 0) (Point 1 1)
+      let s2 = Circle (Point 1 1) 1
+    |} [DAdt (("Point", [("Point", TTuple ([TInt; TInt]))]));
+        DAdt (("Shape",
+               [("Rectangle", TTuple ([TAdt ("Point"); TAdt ("Point")]));
+                ("Circle", TTuple ([TAdt ("Point"); TInt]))]))]
+
+let%test _ = 
+    parse_test {|
+      let surface (Circle _ r) = p * r * r
+      let surface (Rectangle (Point x1 y1) (Point x2 y2)) = abs (x1 - x2) * abs (y1 - y2)
+    |} [DLet ((PVar ("surface"),
+            EFun
+            (PACase ("Circle", PTuple ([PWild; PVar ("r")])),
+             EBinOp (Mul, EVar ("p"), EBinOp (Mul, EVar ("r"), EVar ("r"))))));
+        DLet ((PVar ("surface"),
+            EFun
+            (PACase
+             ("Rectangle",
+              PTuple ([PACase ("Point", PTuple ([PVar ("x1"); PVar ("y1")]));
+                       PACase ("Point", PTuple ([PVar ("x2"); PVar ("y2")]))])),
+             EBinOp
+             (Mul, EApp (EVar ("abs"), EBinOp (Sub, EVar ("x1"), EVar ("x2"))),
+              EApp (EVar ("abs"), EBinOp (Sub, EVar ("y1"), EVar ("y2")))))))]
