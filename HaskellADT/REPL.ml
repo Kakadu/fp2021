bbb4eed42db6bd7f197a6a28ec34d18fba46e415
esvault <54893829+esvault@users.noreply.github.com>
2022-03-22 17:19:58 +0300

    clean up REPL.ml

--- HaskellADT/REPL.ml
+++ HaskellADT/REPL.ml
@@ -3,29 +3,3 @@ open HaskellADT_lib.Parser
 
 (* let x = Var "a" *)
 let () = print_endline "REPL not implemented"
-
-let code = {|
-let x = 2 + 5
-let f x y = x + y
-let p = f 5 10
-let b = (True && (5 > 4))
-let xs = [1,2,3,4]
-let x = "Hello" <> "Hello"
-|}
-
-let funct code = 
-  match run_interpret code with
-  | Ok ok -> (Caml.Format.printf "%a\n" pp_interpret_ok ok) 
-  | Error err -> (Caml.Format.printf "%a\n" pp_interpret_err err)
-;;
-
-let parser code = parse_with (chainr1 pattern popcons) code
-
-(* let (code) = match parser code with
-| Ok ok -> Format.printf "%a\n" pp_prog ok
-| _ -> Format.printf "Error" *)
-
-(* let rez = funct code;; *)
-
-(* parse_or_error {|let f x = case x of 0 -> True 
-                                     1 -> False|} *)
