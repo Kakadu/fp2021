814269e593ce1141eb22153acb2b0e5722e42914
Evgeniy Bakaev <evgeniy.bakaev@compvisionsys.com>
2022-05-02 15:37:20 +0300

    add methodcall, methodef, assign implementations

--- Python/lib/interpreter.ml
+++ Python/lib/interpreter.ml
@@ -35,6 +35,10 @@ module Eval (M : MONADERROR) = struct
   type vars = (identifier, value) Hashtbl.t
   type methods = (identifier, params * statements list) Hashtbl.t
 
+  type scope =
+    | Global
+    | Class of identifier
+
   let rec map f = function
     | [] -> return []
     | h :: tl -> f h >>= fun c -> map f tl >>= fun lst -> return (c :: lst)
@@ -57,7 +61,8 @@ module Eval (M : MONADERROR) = struct
     { local_vars : vars
     ; return_v : value
     ; methods : methods
-    ; classes : (identifier, class_ctx) Hashtbl.t
+    ; scope : scope
+    ; classes : (identifier, class_ctx) Hashtbl.t (* class instances are here too*)
     }
 
   let check_is_already_exist tbl key =
@@ -79,7 +84,7 @@ module Eval (M : MONADERROR) = struct
       | Float f -> return (VFloat f)
       | String s -> return (VString s)
       | Bool b -> return (VBool b))
-    | Var (VarName (_, v)) -> get_var ctx.local_vars v
+    | Var (VarName (_, id)) -> get_var ctx.local_vars id
     | ArithOp (op, e1, e2) ->
       eval_expr ctx e1
       >>= fun l ->
@@ -161,10 +166,27 @@ module Eval (M : MONADERROR) = struct
       | Some mths -> get_var mths.fields field_name
       | None -> error "fail with field access")
     | MethodAccess (instance_name, method_name, args) ->
-      error "asd"
-      (* (match Hashtbl.find_opt ctx.classes instance_name with
-      | Some methd -> eval_method ctx methd
-       *)
+      let push_args_to_local_vars local ctx =
+        let vars = Hashtbl.create 16 in
+        map (fun e -> eval_expr ctx e) args
+        >>= fun vals ->
+        iter2 (fun id v -> return @@ Hashtbl.replace vars id v) local vals
+        >>= fun _ -> return vars
+      in
+      (match Hashtbl.find_opt ctx.classes instance_name with
+      | Some cls ->
+        (match Hashtbl.find_opt cls.methods method_name with
+        | Some methd ->
+          let try_eval_method (params, body) =
+            push_args_to_local_vars params ctx
+            >>= fun vars ->
+            eval_method
+              { ctx with local_vars = vars; scope = Class cls.id; return_v = VNone }
+              body
+          in
+          try_eval_method methd
+        | None -> error "method doesn't exist in class")
+      | None -> error "class instance doesn't exist")
     | MethodCall (method_name, args) ->
       let push_args_to_local_vars local ctx =
         let vars = Hashtbl.create 16 in
@@ -185,13 +207,7 @@ module Eval (M : MONADERROR) = struct
 
   and eval_method ctx = function
     | [] -> return ctx.return_v
-    | [ Return exprs ] ->
-      (match exprs with
-      | [] -> return VNone
-      | [ e ] -> eval_expr ctx e >>= fun res -> return res
-      | _ as expr_list ->
-        map (fun expr -> eval_expr ctx expr) expr_list
-        >>= fun res_list -> return (VList res_list))
+    | [ Return exprs ] -> eval_return exprs ctx
     | stmt :: stmts ->
       eval_stmt ctx stmt
       >>= fun x ->
@@ -200,7 +216,46 @@ module Eval (M : MONADERROR) = struct
       | VNone -> eval_method x stmts
       | v -> return v)
 
-  and eval_stmt ctx stmt = error "not implemented"
+  and eval_return exprs ctx =
+    match exprs with
+    | [] -> return VNone
+    | [ e ] -> eval_expr ctx e >>= fun res -> return res
+    | _ as expr_list ->
+      map (fun expr -> eval_expr ctx expr) expr_list
+      >>= fun res_list -> return (VList res_list)
+
+  and eval_stmt ctx = function
+    | Expression e -> eval_expr ctx e >>= fun v -> return { ctx with return_v = v }
+    | Assign (exprs, vals) ->
+      let set_value_to_var value = function
+        | Var (VarName (Local, id)) -> return (Hashtbl.replace ctx.local_vars id value)
+        | _ -> error "sad"
+      in
+      map (fun expr -> eval_expr ctx expr) vals
+      >>= fun vs -> iter2 set_value_to_var vs exprs >>= fun _ -> return ctx
+    | MethodDef (id, params, stmts) ->
+      let add_method_to_class class_name =
+        get_var ctx.classes class_name
+        >>= fun cur_class ->
+        return (Hashtbl.replace cur_class.methods id (params, stmts))
+        >>= fun _ -> return ctx
+      in
+      (match ctx.scope with
+      | Class class_name -> add_method_to_class class_name >>= fun _ -> return ctx
+      | Global ->
+        return (Hashtbl.replace ctx.methods id (params, stmts)) >>= fun _ -> return ctx)
+    | If _ -> error "not implemented"
+    | Else _ -> error "not implemented"
+    | While _ -> error "not implemented"
+    | For _ -> error "not implemented"
+    | Return exprs -> eval_return exprs ctx >>= fun v -> return { ctx with return_v = v }
+    | _ -> error "PARSER FAIL"
+
+  and eval_prog ctx = function
+    | [] -> return VNone
+    | [ x ] -> eval_stmt ctx x >>= fun ctx_upd -> return ctx_upd.return_v
+    | stmt :: stmts -> eval_stmt ctx stmt >>= fun ctx_upd -> eval_prog ctx_upd stmts
+  ;;
 
   let init_global_ctx () =
     let methods = Hashtbl.create 16 in
@@ -212,22 +267,64 @@ module Eval (M : MONADERROR) = struct
     in
     { local_vars = Hashtbl.create 16
     ; methods
+    ; scope = Global
     ; classes = Hashtbl.create 16
     ; return_v = VNone
     }
   ;;
 
+  let init_test_ctx () =
+    let methods = Hashtbl.create 16 in
+    let add_test_method =
+      Hashtbl.add
+        methods
+        "a"
+        ([], [ Return [ ArithOp (Add, Const (Integer 5), Const (Integer 4)) ] ])
+    in
+    let local_vars = Hashtbl.create 16 in
+    let add_test_var =
+      Hashtbl.add local_vars "x" (VInt 5);
+      Hashtbl.add local_vars "y" (VInt 6)
+    in
+    { local_vars; methods; scope = Global; classes = Hashtbl.create 16; return_v = VNone }
+  ;;
+
   let set_var id v ctx = Hashtbl.replace ctx.local_vars id v
-  let test_ctx = init_global_ctx ()
+  let global_ctx = init_global_ctx ()
+  let test_ctx = init_test_ctx ()
 end
 
 open Eval (Result)
 
 let%test _ =
   eval_expr
-    test_ctx
+    global_ctx
     (ArithOp (Add, ArithOp (Add, Const (Integer 5), Const (Integer 4)), Const (Integer 4)))
   = Result.return (VInt 13)
 ;;
 
-let%test _ = eval_expr test_ctx (MethodCall ("a", [])) = Result.return (VInt 9)
+let%test _ = eval_expr global_ctx (MethodCall ("a", [])) = Result.return (VInt 9)
+
+let%test _ =
+  eval_prog
+    global_ctx
+    [ Assign
+        ( [ Var (VarName (Local, "x")); Var (VarName (Local, "y")) ]
+        , [ Const (Integer 5); Const (Integer 6) ] )
+    ]
+  = Result.return VNone
+;;
+
+let%test _ =
+  eval_prog
+    global_ctx
+    [ MethodDef
+        ( "sum"
+        , [ "x"; "y" ]
+        , [ Return
+              [ ArithOp (Add, Var (VarName (Local, "x")), Var (VarName (Local, "y"))) ]
+          ] )
+    ; Return [ MethodCall ("sum", [ Const (Integer 5); Const (Integer 5) ]) ]
+    ]
+  = Result.return (VInt 10)
+;;
